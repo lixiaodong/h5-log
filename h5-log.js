d9e140ea9db993f12df7dc32a9c389643be66da4
李小东 <xiaodongl@moguyun.com>
2017-10-17 16:39:13 +0800

    add refresh button to reload page

--- h5-log.js
+++ h5-log.js
@@ -9,6 +9,7 @@
 var H5Log = {
     container:null,
     btnH5Log:null,
+    toolBar:null,
     maxCacheSize:1000,//最大缓存大小
     getItem:function () {
 
@@ -16,20 +17,30 @@ var H5Log = {
     setItem:function (k,v) {
 
     },
+    showElement:function () {
+
+    },
     init:function () {
         if(!this.debug){return;}
-        document.body.innerHTML += '<div class="h5-log-close" style="display:none;position: fixed;z-index: 9999; font-size: 14px; color: white; bottom: 60%; right: 0px; background: #000333; padding: 5px;">关闭</div>';
+
+        var html = '<div class="h5-log-tool-bar" style="display: none;position: fixed;z-index: 9999;font-size: 14px;color: white;bottom: 420px;right: 0px;background: rgb(0, 3, 51);padding: 5px;">';
+        html += '<span class="h5-log-close" style="">关闭</span>';
+        html += '<span class="h5-log-refresh" onclick="location.reload(true)" style="position: fixed;z-index: 9999; font-size: 14px; color: white; bottom: 420px; right: 40px; background: #000333; padding: 5px;">刷新</span>';
+        html += '</div>';
+        document.body.innerHTML += html;
+
         document.body.innerHTML += '<div class="h5-log-container twisterInUp magictime" style="padding:10px;overflow:auto;word-break:break-all;display: none;width: 100%;height: 60%;position: fixed;z-index: 9999;bottom: 0;right: 0;background: rgba(0,0,0,0.6);color: white;font-size: 12px;"></div>';
         document.body.innerHTML += '<div class="btn-h5-log" style="position: fixed; width: 50px; height: 50px; right: 10px; bottom: 50px; background: green; border-radius: 50%; line-height: 50px; font-size: 16px; text-align: center; color: chartreuse; z-index: 9999;">日志</div>';
         document.head.innerHTML += '<style> .tinRightIn { animation-name: tinRightIn } @keyframes tinRightIn { 0% { opacity: 0; transform: scale(1, 1) translateX(900%) } 50%, 70%, 90% { opacity: 1; transform: scale(1.1, 1.1) translateX(0) } 100%, 60%, 80% { opacity: 1; transform: scale(1, 1) translateX(0) } } .tinRightOut { animation-name: tinRightOut } @keyframes tinRightOut { 0%, 20%, 40%, 50% { opacity: 1; transform: scale(1,1) translateX(0) } 10%, 30% { opacity: 1; transform: scale(1.1, 1.1) translateX(0) } 100% { opacity: 0; transform: scale(1, 1) translateX(900%) } } .magictime { animation-duration: 1s; animation-fill-mode: both } </style>';
         this.container = document.querySelector('.h5-log-container');
         this.btnH5Log = document.querySelector('.btn-h5-log');
+        this.toolBar = document.querySelector('.h5-log-tool-bar');
         var btnClose = document.querySelector('.h5-log-close');
         var that = this;
         this.btnH5Log.onclick = function () {
             that.container.style.display = 'block';
             that.btnH5Log.style.display = 'none';
-            btnClose.style.display = 'block';
+            that.toolBar.style.display = 'block';
             this.style.className = 'h5-log-close';
             that.container.className = 'h5-log-container tinRightIn magictime';
         }
@@ -37,7 +48,7 @@ var H5Log = {
         btnClose.onclick = function () {
             // that.container.style.display = 'none';
             that.btnH5Log.style.display = 'block';
-            this.style.display = 'none';
+            that.toolBar.style.display = 'none';
             this.style.className = 'tinRightOut magictime';
             that.container.className = 'tinRightOut magictime';
         }
